50e5f4e2ad9d899e2c6e49d3f61fcb94635edd3d
zhsir123 <zhsir123@gmail.com>
2017-09-24 22:49:24 +0900

    멍청한 strip 수정
    멍청한 strip 수정

--- renamer.py
+++ renamer.py
@@ -7,16 +7,22 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--before', help='before string', required=True)
     parser.add_argument('-a', '--after', default="", help='after string defualt is "" ')
     parser.add_argument('-p', '--path', help="directory path", required=True)
+    parser.add_argument('-s', '--strip', type=bool, help="after replace use strip")
 
     args = parser.parse_args()
 
     path = args.path
-    
-    files = os.listdir(path)
-    for file in files:
-        if args.before in file:
-            before = os.path.join(path, file)
-            after = os.path.join(path, file.replace(args.before, args.after).strip())
+                
+    files = [os.path.splitext(file) for file in os.listdir(path)]
+    for name, fmt in files:
+        if args.before in name:
+            before = os.path.join(path, name + fmt)
+            after = name.replace(args.before, args.after).strip()
+            if args.strip:
+                after = os.path.join(path, after.strip())
+            else:
+                after = os.path.join(path, after)
+            
             os.rename(before, after)
             print(before + " => " + after)
             
